0a4217530f94dedf8950a781e4b75217a291510f
tina123 <269368253@qq.com>
2019-12-18 21:41:48 +0800

    第一次提交

---
+++ demo1/src/plugins/ele.js
@@ -0,0 +1,3 @@
+import Vue from 'vue';
+import { Tag } from 'element-ui';
+Vue.use(Tag)
---
+++ demo1/src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <el-tag>标签一</el-tag>
+    <el-tag type="success">标签二</el-tag>
+    <el-tag type="info">标签三</el-tag>
+    <el-tag type="warning">标签四</el-tag>
+    <el-tag type="danger">标签五</el-tag>
+  </div>
+</template>
